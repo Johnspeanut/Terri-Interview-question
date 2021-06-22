d30f9b356dc49a5c73c6b355a0ea105dd685a09e
johnspeanut <pengqiong2015fall@hotmail.com>
2021-06-22 11:17:50 -0700

    update experiment

--- main.py
+++ main.py
@@ -25,14 +25,18 @@ def main():
 
     # Do some experiments from [1,1,1,1] to [13,13,13,13]. What's the
     # probability that the problme is solvable.
-    TOTAL_COMBINATION = 28561
+    TOTAL_COMBINATION = 270725
+    deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
     countNumber = 0
     listOfCards = []
-    for i in range(1, 14):
-        for j in range(1, 14):
-            for k in range(1, 14):
-                for x in range(1, 14):
-                    cards = [i, j, k, x]
+    for i in range(len(deck)):
+        for j in range(i+1, len(deck)):
+            for k in range(j+1, len(deck)):
+                for x in range(k+1, len(deck)):
+                    cards = [deck[i], deck[j], deck[k], deck[x]]
                     listOfCards.append(cards)
 
     for i in range(len(listOfCards)):
@@ -41,7 +45,7 @@ def main():
     print("Of " + str(TOTAL_COMBINATION) + ", there are " + str(countNumber) +
           " that are solvable." + " And the probability is " +
           str(float(countNumber)/TOTAL_COMBINATION))
-    
+    # 218265 Of 270725 are solvable. And the probability is 0.806.
 
 
 if __name__ == "__main__":
